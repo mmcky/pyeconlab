c1cafb2fd1b0f24f2b27cda73606e4350a0f4a3f
SanguineTurtle <mamckay@gmail.com>
2014-07-07 16:27:05 +1000

    Reshape Folders to package format

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+  name = 'pyeconlab',
+  packages = ['pyeconlab'], 
+  version = '0.1-alpha',
+  description = 'Python Package For Economists',
+  author = 'Matthew McKay',
+  author_email = 'mamckay@gmail.com',
+  url = 'https://github.com/sanguineturtle/pyeconlab',
+  download_url = 'https://github.com/sanguineturtle/pyeconlab/tarball/0.1-alpha', 
+  keywords = ['quantitative', 'economics', 'international trade', 'complexity'] 
+)
