631a1cb776589ce7d6d79a5178b55e0a1498e539
SanguineTurtle <mamckay@gmail.com>
2014-08-04 17:07:35 +1000

    Updated Docstrings and file logic

--- pyeconlab/util/convert.py
+++ pyeconlab/util/convert.py
@@ -6,11 +6,25 @@ import pandas as pd
 
 def from_series_to_pyfile(series, target_dir='data/', fl=None, docstring=None):
 	"""
-	Construct a .py file from a Pandas Series Object
+	Construct a .py file from a Pandas Series Object 
+	[Warning: Ensure `series` is named what you would like the variable to be]
+
+	Arguments
+	---------
+	series 		: 	pd.Series (that has an index)
+	target_dir 	: 	target directory to save file
+	fl 			: 	filname
+	docstring 	:	specify a docstring at the top of the file	 
+
+	Example
+	-------
 
 	s = pd.Series([1,2,3,4])
 
-	will write a file with:  s.name = [1,2,3,4]
+	will write a file with:  s.name = [	1,
+										2,
+										...
+									  ]
 	"""
 	if type(series) != pd.Series:
 		raise TypeError("series: must be a pd.Series")
@@ -24,20 +38,35 @@ def from_series_to_pyfile(series, target_dir='data/', fl=None, docstring=None):
 			tabs = 4
 		items += '\t'*tabs + '\'' + '%s'%item + '\''  + ',' + '\n'
 	doc = doc_string + items[:-1] + ']\n'
-	if fl == None:
-		return doc	 
-	else:
+	if type(fl) == str:
 		# Write to Disk #
 		f = open(target_dir+fl, 'w')
 		f.write(doc)
-		f.close()
+		f.close()			 
+	else:
+		return doc
+
 
 
 def from_idxseries_to_pydict(series, target_dir='data/', fl=None, docstring=None):
 	"""
 	Construct a .py file containing a Dictionary from an Indexed Pandas Series Object
+	[Warning: Ensure `series` is named what you would like the variable to be]
 
-	s.name = { index : value }
+	Arguments
+	---------
+	series 		: 	pd.Series (that has an index)
+	target_dir 	: 	target directory to save file
+	fl 			: 	filname
+	docstring 	:	specify a docstring at the top of the file	 
+	fl 	:  filename
+
+	Example
+	-------
+
+	s.name = { 	index : value,
+				... etc.
+			 }
 	"""
 	if type(series) != pd.Series:
 		raise TypeError("series: must be a pd.Series with an Index")
@@ -53,10 +82,11 @@ def from_idxseries_to_pydict(series, target_dir='data/', fl=None, docstring=None
 			tabs = 2
 		items += '\t'*tabs + '\'' + '%s'%idx + '\''  + ' : ' + '\'' + '%s'%val + '\'' + ',' + '\n'
 	doc = docstring + items + '}\n'
-	if fl == None:
-		return doc	 
-	else:
+	if type(fl) == str:
 		# Write to Disk #
 		f = open(target_dir+fl, 'w')
 		f.write(doc)
 		f.close()
+	else:
+		return doc	
+		
