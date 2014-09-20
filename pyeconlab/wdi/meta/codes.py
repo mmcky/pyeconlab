e214d36387f9a08688580d1a58899f0c1a496c12
SanguineTurtle <mamckay@gmail.com>
2014-09-20 17:59:03 +1000

    Adding some old utitlities, and meta data for wdi

---
+++ pyeconlab/wdi/meta/codes.py
@@ -0,0 +1,20 @@
+"""
+Meta Subpackage for WDI
+"""
+
+
+class WDISeriesCodes():
+	'''
+		Container for WDI Series Codes (World Bank)
+	'''
+	GDP = r'NY.GDP.MKTP.CD'
+	GDPPC = r'NY.GDP.PCAP.CD'
+	GDPGrowth = r'NY.GDP.MKTP.KD.ZG'
+	GDPPCGrowth = r'NY.GDP.PCAP.KD.ZG'
+
+CodeToName = {
+	r'NY.GDP.MKTP.CD' 		: 'GDP',
+	r'NY.GDP.PCAP.CD' 		: 'GDPPC',
+	r'NY.GDP.MKTP.KD.ZG' 	: 'GDPGrowth',
+	r'NY.GDP.PCAP.KD.ZG' 	: 'GDPPCGrowth',
+}
