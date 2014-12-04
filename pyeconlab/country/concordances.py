967acc45744b77695b7b74bd9757a5aa0c0aa2a0
SanguineTurtle <matt.mckay@me.com>
2014-12-04 18:20:48 +1100

    worked on documentation

--- pyeconlab/country/concordances.py
+++ pyeconlab/country/concordances.py
@@ -1,88 +1,149 @@
 """
 Country Concordances
+====================
 
-Simple Wrappers for Quickly Obtaining a Country Concordane
+Simple Wrappers for Quickly Obtaining a Country Concordance
 
 Available:
 ---------
-iso3c_to_iso3n
-iso3n_to_iso3c
-iso3n_to_countryname
+
+#. iso3c_to_iso3n
+#. iso3n_to_iso3c
+#. iso3n_to_countryname
 
 Future Work
 -----------
-[1] Write a Decorator Function to Sort out source_institution?
-[2] Currently iso3c and iso3n are being used as descriptors rather than source of origin
-	Maybe source_institution is unnecessary and there should just be a lot of specific concordances
-	un_iso3c_to_un_countryname OR un_iso3c_to_countryname
-	and 
-	iso3c_to_iso3n is reserved for ISO 3166.xml File
+
+#. Write a Decorator Function to Sort out source_institution (?)
+#. Maybe ``source_institution`` is unnecessary and there should just be a lot of specific concordances
+
+    - un_iso3c_to_un_countryname
+    - un_iso3c_to_countryname
+    - iso3c_to_iso3n is reserved for ISO 3166.xml File
 
 """
 
 from .un import UNCountryCodes
 
+#-Obtain Data Functions-#
+
 def concordance_data(source_institution, verbose='False'):
-	"""
-	Select source_institution and return data object
-	"""
-	if source_institution == 'un':
-			return UNCountryCodes(verbose=verbose)
-	else:
-		raise ValueError("source_institution: only 'un' is currently implemented")
+    """
+    Select source_institution and return data object
+
+    Parameters
+    ----------
+    source_institution  :   string
+                            A string which describes the source_institution as these lists can vary between source institutions
+    verbose             :   string, option(default='False')
+
+    Returns
+    -------
+    CountryCodeObject   :   Object
+
+    """
+    if source_institution == 'un':
+            return UNCountryCodes(verbose=verbose)
+    else:
+        raise ValueError("source_institution: only 'un' is currently implemented")
 
 #-Functions-#
 
 def iso3c_to_iso3n(source_institution='un'):
-	"""
-	ISO3C to ISO3N Mapping Dictionary
+    """
+    Obtain an ISO3C to ISO3N Mapping Dictionary
 
-	source_institution 		: 	[Default: 'un']
+    Parameters
+    ----------
+    source_institution  :   string, optional(default='un')
+                            A string which describes the source_institution as these lists can vary between source institutions
 
-	Notes: 	This will reimport the data from the package xls file but makes a concordance very accessible 
-			If using a lot it might be better to import the UNCountryCodes object instead
+    Returns
+    -------
+    iso3c_to_iso3n      :   dict(string : int)
+                            A dictionary indexed by iso3c and returns iso3n
 
-	Future Work
-	-----------
-	[1] Have the Option to Import from a meta/iso3c_to_iso3n.py file? Current Execution time is ~20ms
-	"""
-	return concordance_data(source_institution, verbose=False).iso3c_to_iso3n
+    Notes  
+    -----
+    This will reimport the data from the package xls file but makes a concordance very accessible. 
+    If using a lot it might be better to import the UNCountryCodes object instead and query that object
+
+    Examples
+    --------
+
+    >>> import pyeconlab
+    >>> c = pyeconlab.country.concordances.iso3c_to_iso3n()
+    >>> print c['AUS']
+    36 
+
+    ..  Future Work
+        -----------
+        [1] Have the Option to Import from a meta/iso3c_to_iso3n.py file? Current Execution time is ~20ms
+    """
+    return concordance_data(source_institution, verbose=False).iso3c_to_iso3n
 
 def iso3c_to_name(source_institution='un'):
-	"""
-	ISO3N to ISO3C Mapping Dictionary
-	
-	Notes: 	This will reimport the data from the package xls file  but makes a concordance very accessible 
-			If using a lot it might be better to import the UNCountryCodes object instead
+    """
+    Obtain an ISO3N to ISO3C Mapping Dictionary
 
-	Future Work
-	-----------
-	[1] Have the Option to Import from a meta/iso3c_to_iso3n.py file? Current Execution time is ~20ms
-	"""
-	return concordance_data(source_institution, verbose=False).iso3c_to_name
+    Parameters
+    ----------
+    source_institution  :   string, optional(default='un')
+                            A string which describes the source_institution as these lists can vary between source institutions
+
+    Returns
+    -------
+    iso3c_to_name       :   dict(string : string)
+                            A dictionary indexed by iso3c and returns a country name
+    
+    Notes  
+    -----
+    This will reimport the data from the package xls file  but makes a concordance very accessible. 
+    If using a lot it might be better to import the UNCountryCodes object instead and query that object
+
+    """
+    return concordance_data(source_institution, verbose=False).iso3c_to_name
 
 def iso3n_to_iso3c(source_institution='un'):
-	"""
-	ISO3N to ISO3C Mapping Dictionary
-	
-	Notes: 	This will reimport the data from the package xls file  but makes a concordance very accessible 
-			If using a lot it might be better to import the UNCountryCodes object instead
+    """
+    ISO3N to ISO3C Mapping Dictionary
+    
+    Parameters
+    ----------
+    source_institution  :   string, optional(default='un')
+                            A string which describes the source_institution as these lists can vary between source institutions
 
-	Future Work
-	-----------
-	[1] Have the Option to Import from a meta/iso3c_to_iso3n.py file? Current Execution time is ~20ms
-	"""
-	return concordance_data(source_institution, verbose=False).iso3n_to_iso3c
+    Returns
+    -------
+    iso3n_to_iso3c      :   dict(int : string)
+                            A dictionary indexed by iso3n and returns iso3c
+
+    Notes  
+    -----
+    This will reimport the data from the package xls file  but makes a concordance very accessible.
+    If using a lot it might be better to import the UNCountryCodes object instead and query that object
+
+    """
+    return concordance_data(source_institution, verbose=False).iso3n_to_iso3c
 
 def iso3n_to_name(source_institution='un'):
-	"""
-	ISO3N to ISO3C Mapping Dictionary
-	
-	Notes: 	This will reimport the data from the package xls file  but makes a concordance very accessible 
-			If using a lot it might be better to import the UNCountryCodes object instead
+    """
+    ISO3N to ISO3C Mapping Dictionary
+    
+    Parameters
+    ----------
+    source_institution  :   string, optional(default='un')
+                            A string which describes the source_institution as these lists can vary between source institutions
 
-	Future Work
-	-----------
-	[1] Have the Option to Import from a meta/iso3c_to_iso3n.py file? Current Execution time is ~20ms
-	"""
-	return concordance_data(source_institution, verbose=False).iso3n_to_name
+    Returns
+    -------
+    iso3n_to_name      :    dict(int : string)
+                            A dictionary indexed by iso3n and returns a country name
+
+    Notes  
+    -----
+    This will reimport the data from the package xls file  but makes a concordance very accessible. 
+    If using a lot it might be better to import the UNCountryCodes object instead
+
+    """
+    return concordance_data(source_institution, verbose=False).iso3n_to_name
--- pyeconlab/country/un.py
+++ pyeconlab/country/un.py
@@ -1,104 +1,135 @@
 """
 United Nations Country Information
+==================================
 
-Contains
---------
-UNCountryCodes 	: 	Wrapper for UN Country Codes Data (ISO3C, ISO3N, Names)
+This module contains UN country code information and Data (ISO3C, ISO3N, and Names)
 
 """
 
+#-External Imports-#
 import sys
 import pandas as _pd
 import pyeconlab.util as _util
 
+#-Package Imports-#
 from .base_countrycodes import CountryCodes
 
 class UNCountryCodes(CountryCodes):
-	"""
-	United Nations Country Code Classifications
+    """
+    United Nations Country Code Classifications
 
-	DataSource:
-	----------
-	./data/unstats_CountryCodeAndNameToISO2ISO3.xls (md5hash = 332efad5c0c03064658fbd35c40646b0)
-		Source: 			http://unstats.un.org/unsd/tradekb/Knowledgebase/Comtrade-Country-Code-and-Name
-		Downloaded: 		22-July-2014
-		Original FileName: 	Country code and Name ISO3 ISO3.xls
-		Original md5hash: 	acf6f39d2a49e1611e929a778a136706
+    **DataSource**: ::
 
-	File Interface:
-	---------------
-	[Country Code, Country Name English, Country Fullname English, Country Abbrevation, Cty Comments,
-		ISO2-digit Alpha, ISO3-digit Alpha, Start Valid Year, End Valid Year]
+        Package Location:   ./data/unstats_CountryCodeAndNameToISO2ISO3.xls (md5hash = 332efad5c0c03064658fbd35c40646b0)
+        Source:             http://unstats.un.org/unsd/tradekb/Knowledgebase/Comtrade-Country-Code-and-Name
+        Downloaded:         22-July-2014
+        Original FileName:  Country code and Name ISO3 ISO3.xls
+        Original md5hash:   acf6f39d2a49e1611e929a778a136706
 
-	Recodes:
-	--------
-	Country Code 			-> iso3n 				#UN CountryCode = iso3n
-	Country Name English 	-> countryname 
-	Country Fullname English -> countryfullname
-	ISO2-digit Alpha 		-> iso2c
-	ISO3-digit Alpha		-> iso3c
-	Start Valid Year 		-> startyear
-	End Valid Year			-> endyear
+        File Interface:
+        ---------------
+        [Country Code, Country Name English, Country Fullname English, Country Abbrevation, Cty Comments,
+            ISO2-digit Alpha, ISO3-digit Alpha, Start Valid Year, End Valid Year]
 
-	Dropping: 
-	--------
-	Country Abbrevation, Cty Comments
+        Recodes:
+        --------
+        Country Code            -> iso3n                #UN CountryCode = iso3n
+        Country Name English    -> countryname 
+        Country Fullname English -> countryfullname
+        ISO2-digit Alpha        -> iso2c
+        ISO3-digit Alpha        -> iso3c
+        Start Valid Year        -> startyear
+        End Valid Year          -> endyear
 
-	Future Work
-	-----------
-	[1] Meta Data Constructor for ./meta/
+        Dropping: 
+        --------
+        Country Abbrevation, Cty Comments
 
-	"""
+    Notes
+    -----
+    This class inherits methods from ``CountryCodes``
 
-	drop 	= [
-		u'Country Abbrevation',
-		u'Cty Comments'
-	]
+    Examples
+    --------
 
-	recodes = {
-		u'Country Code' 			: 'iso3n',
-		u'Country Name English' 	: 'countryname',
-		u'Country Fullname English' : 'countryfullname',
-		u'ISO2-digit Alpha'			: 'iso2c',
-		u'ISO3-digit Alpha' 		: 'iso3c',
-		u'Start Valid Year'			: 'startyear',
-		u'End Valid Year'			: 'endyear',
-	}
+    >>> from pyeconlab.country import UNCountryCodes
+    >>> c = UNCountryCodes()
+    [INFO] Loaded UN Country Codes from file unstats_CountryCodeAndNameToISO2ISO3.xls 
+    Location: C:\Users\Matt-Work\Anaconda\lib\site-packages\pyeconlab\country\data\unstats_CountryCodeAndNameToISO2ISO3.xls
+    Dropping: Country Abbrevation
+    Dropping: Cty Comments
+    [Recode Column Summary]
+    # of Column Items: 7
+    # of Recode Items: 7
+    Updates: 
+    ------- 
+      ISO2-digit Alpha -> iso2c
+      Start Valid Year -> startyear
+      Country Code -> iso3n
+      Country Fullname English -> countryfullname
+      ISO3-digit Alpha -> iso3c
+      Country Name English -> countryname
+      End Valid Year -> endyear
+    ------- 
+    Number of Changes: 7
+    >>> print c.iso3c_to_iso3n['AUS']
+    36
 
-	def __init__(self, verbose=True):
-		"""
-		Initialise Class and Populate with Data From Package
+    ..  Future Work
+        -----------
+        [1] Meta Data Constructor for ./meta/
 
-		Future Work
-		-----------
-		[1] Allow specification of User File
-		"""
-		# - Attributes - #
-		self._fn 		= u"unstats_CountryCodeAndNameToISO2ISO3.xls"
-		if sys.platform.startswith('win'):
-			self._md5hash 	= "57f8ffd8638e77354c312823f13cb867"						#Why does this keep changing?
-		else:
-			self._md5hash 	= "0ae80063248db7a9446d155c1360345d" 						#This is the md5sum on the mac?
-		self._fl 		= _util.package_folder(__file__, "data") + self._fn
+    """
 
-			# --> This Requires Debugging <-- #
-			#
-			# if _util.verify_md5hash(self._fl, self._md5hash):
-			# 	self.data 	= _pd.read_excel(self._fl, )
-			# else:
-			# 	raise ValueError("Object's md5hash (%s) doesn't match the md5hash of the package data file!" % self._md5hash)
-			#
-			# ---> END Debugging < -- #
-		# - Acquire Data From Package - #
-		self.data 	= _pd.read_excel(self._fl)
-		if verbose: print '[INFO] Loaded UN Country Codes from file %s \nLocation: %s\n' % (self._fn, self._fl)
+    drop    = [
+        u'Country Abbrevation',
+        u'Cty Comments'
+    ]
 
-		# - Drop Data - #
-		for item in self.drop:
-			if verbose: print "Dropping: %s" % (item)
-			del self.data[item]
-		if verbose: print ""
+    recodes = {
+        u'Country Code'             : 'iso3n',
+        u'Country Name English'     : 'countryname',
+        u'Country Fullname English' : 'countryfullname',
+        u'ISO2-digit Alpha'         : 'iso2c',
+        u'ISO3-digit Alpha'         : 'iso3c',
+        u'Start Valid Year'         : 'startyear',
+        u'End Valid Year'           : 'endyear',
+    }
 
-		# - Recode Data - #
-		self.data = _util.recode_index(self.data, self.recodes, axis='columns', verbose=verbose)
+    def __init__(self, verbose=True):
+        """
+        Initialise Class and Populate with Data From Package
+
+        ..  Future Work
+            -----------
+            [1] Allow specification of User File
+        """
+        # - Attributes - #
+        self._fn        = u"unstats_CountryCodeAndNameToISO2ISO3.xls"
+        if sys.platform.startswith('win'):
+            self._md5hash   = "57f8ffd8638e77354c312823f13cb867"                        #Why does this keep changing?
+        else:
+            self._md5hash   = "0ae80063248db7a9446d155c1360345d"                        #This is the md5sum on the mac?
+        self._fl        = _util.package_folder(__file__, "data") + self._fn
+
+            # --> This Requires Debugging <-- #
+            #
+            # if _util.verify_md5hash(self._fl, self._md5hash):
+            #   self.data   = _pd.read_excel(self._fl, )
+            # else:
+            #   raise ValueError("Object's md5hash (%s) doesn't match the md5hash of the package data file!" % self._md5hash)
+            #
+            # ---> END Debugging < -- #
+        # - Acquire Data From Package - #
+        self.data   = _pd.read_excel(self._fl)
+        if verbose: print '[INFO] Loaded UN Country Codes from file %s \nLocation: %s\n' % (self._fn, self._fl)
+
+        # - Drop Data - #
+        for item in self.drop:
+            if verbose: print "Dropping: %s" % (item)
+            del self.data[item]
+        if verbose: print ""
+
+        # - Recode Data - #
+        self.data = _util.recode_index(self.data, self.recodes, axis='columns', verbose=verbose)
 
