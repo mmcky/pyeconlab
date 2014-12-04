e1cab0cf270f642e9b5dce859e99dab636330b68
SanguineTurtle <mamckay@gmail.com>
2014-12-04 15:00:14 +1100

    Updating docstring

--- pyeconlab/country/base_countrycodes.py
+++ pyeconlab/country/base_countrycodes.py
@@ -2,74 +2,74 @@
 Generic Country Code Classes and Methods
 """
 
+__docformat__ = 'reStructuredText'
+
 import pandas as _pd
 
 class CountryCodes(object):
-	"""
-	SuperClass for CountryCode Datasets (such as in un.py)
-	Contains Common Properties and Methods for CountryCode Objects
+    """
+    SuperClass for CountryCode Datasets (such as in un.py).
+    Contains Common Properties and Methods for CountryCode Objects
 
-	Interface
-	---------
-	data attribute must contain => ['iso3c', 'iso3n', 'countryname']
+    Notes
+    -----
+    Interface: data attribute must contain => ['iso3c', 'iso3n', 'countryname']
 
-	Notes:
-	-----
-	[1] For country data that isn't standard (like iso3c, iso3n etc.) 
-		then individual classes or methods will need to be implemented in a Child Object.
+    #. For country data that isn't standard (like iso3c, iso3n etc.) then individual classes or methods will need to be implemented in a Child Object.
 
-	Future Work
-	-----------
-	[1] Improve Error Handling
-	[2] Improve interface enforcement
-	"""
-	
-	data = _pd.DataFrame
-	interface = ['iso3c', 'iso3n', 'countryname']
+    Future Work
+    -----------
+    1. Improve Error Handling
+    2. Improve interface enforcement
 
-	#-Properties-#
-	@property 
-	def num_iso3c(self):
-		return len(self.data['iso3c'].dropna())
+    """
 
-	#-Series Properties-#
+    data = _pd.DataFrame
+    interface = ['iso3c', 'iso3n', 'countryname']
 
-	@property 
-	def iso3c(self):
-		return list(self.data['iso3c'].dropna()) 			#Some Countries don't have official iso3c codes in some incomplete datasets
+    #-Properties-#
+    @property 
+    def num_iso3c(self):
+        return len(self.data['iso3c'].dropna())
 
-	@property
-	def iso3n(self):
-		return list(self.data['iso3n'].dropna())
+    #-Series Properties-#
 
-	#-Generate Concordance Dictionaries-#
+    @property 
+    def iso3c(self):
+        return list(self.data['iso3c'].dropna())            #Some Countries don't have official iso3c codes in some incomplete datasets
 
-	@property 
-	def name_to_iso3c(self):
-		concord = self.data[['iso3c', 'countryname']].dropna().set_index('countryname') 	#Drop Codes with No Pair
-		return concord['iso3c'].to_dict()
+    @property
+    def iso3n(self):
+        return list(self.data['iso3n'].dropna())
 
-	@property 
-	def iso3c_to_name(self):
-		concord = self.data[['iso3c', 'countryname']].dropna().set_index('iso3c') 			#Drop Codes with No Pair
-		return concord['countryname'].to_dict()
+    #-Generate Concordance Dictionaries-#
 
-	@property 
-	def iso3n_to_iso3c(self):
-		concord = self.data[['iso3c', 'iso3n']].dropna().set_index('iso3n') 				#Drop Codes with No Pair
-		return concord['iso3c'].to_dict()
+    @property 
+    def name_to_iso3c(self):
+        concord = self.data[['iso3c', 'countryname']].dropna().set_index('countryname')     #Drop Codes with No Pair
+        return concord['iso3c'].to_dict()
 
-	@property 
-	def iso3c_to_iso3n(self):
-		concord = self.data[['iso3c', 'iso3n']].dropna().set_index('iso3c') 				#Drop Codes with No Pair
-		return concord['iso3n'].to_dict()
+    @property 
+    def iso3c_to_name(self):
+        concord = self.data[['iso3c', 'countryname']].dropna().set_index('iso3c')           #Drop Codes with No Pair
+        return concord['countryname'].to_dict()
 
-	@property 
-	def name_to_iso3n(self):
-		concord = self.data[['iso3n', 'countryname']].dropna().set_index('countryname') 	#Drop Codes with No Pair
-		return concord['iso3n'].to_dict()
+    @property 
+    def iso3n_to_iso3c(self):
+        concord = self.data[['iso3c', 'iso3n']].dropna().set_index('iso3n')                 #Drop Codes with No Pair
+        return concord['iso3c'].to_dict()
 
-	@property 
-	def iso3n_to_name(self):
-		concord = self.data[['iso3n', 'countryname']].dropna().set_index('iso3n') 			#Drop Codes with No Pair
-		return concord['countryname'].to_dict()
+    @property 
+    def iso3c_to_iso3n(self):
+        concord = self.data[['iso3c', 'iso3n']].dropna().set_index('iso3c')                 #Drop Codes with No Pair
+        return concord['iso3n'].to_dict()
+
+    @property 
+    def name_to_iso3n(self):
+        concord = self.data[['iso3n', 'countryname']].dropna().set_index('countryname')     #Drop Codes with No Pair
+        return concord['iso3n'].to_dict()
+
+    @property 
+    def iso3n_to_name(self):
+        concord = self.data[['iso3n', 'countryname']].dropna().set_index('iso3n')           #Drop Codes with No Pair
+        return concord['countryname'].to_dict()
