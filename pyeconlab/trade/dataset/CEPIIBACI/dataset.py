cac28452e54ba538b893c5748c8c42ecc8ab24a6
SanguineTurtle <matt.mckay@me.com>
2014-12-08 13:58:42 +1100

    update to spaces

--- pyeconlab/trade/dataset/CEPIIBACI/dataset.py
+++ pyeconlab/trade/dataset/CEPIIBACI/dataset.py
@@ -17,9 +17,9 @@ Product Classification
 
 Types
 -----
-1. Trade Data 	(Bilateral Trade Flows)
-2. Export Data 	(Export Trade Flows)
-3. Import Data 	(Import Trade Flows)
+1. Trade Data   (Bilateral Trade Flows)
+2. Export Data  (Export Trade Flows)
+3. Import Data  (Import Trade Flows)
 """
 
 import numpy as np
@@ -30,113 +30,113 @@ import cPickle as pickle
 from .base import BACI
 from pyeconlab.trade.dataset import CPTradeDataset, CPTradeData, CPExportData, CPImportData
 
-#-These are Just Wrappers to Put a more appropriate Class Name Around Generic Dataset Objects and to provide source details	-#
-#-May want to hide some source details as they clutter the . namespace 														-#
+#-These are Just Wrappers to Put a more appropriate Class Name Around Generic Dataset Objects and to provide source details -#
+#-May want to hide some source details as they clutter the . namespace                                                      -#
 
 #-Trade-#
 
 class BACITradeData(BACI, CPTradeData):
-	""" 
-	BACI TRADE Dataset Wrapper
+    """ 
+    BACI TRADE Dataset Wrapper
 
-	See Also
-	--------
-	pyeconlab.trade.dataset.CEPIIBACI.base
-	pyeconlab.trade.dataset.CPTradeData
-	
-	"""
-	__data = pd.DataFrame()
+    See Also
+    --------
+    pyeconlab.trade.dataset.CEPIIBACI.base
+    pyeconlab.trade.dataset.CPTradeData
+    
+    """
+    __data = pd.DataFrame()
 
-	#-Class Properties-#
+    #-Class Properties-#
 
-	@property 
-	def data(self):
-		return self.__data
-	# @data.setter
-	# def data(self, values):
-	# 	self.__data = values
+    @property 
+    def data(self):
+        return self.__data
+    # @data.setter
+    # def data(self, values):
+    #   self.__data = values
 
-	def set_data(self, value, force=False):
-		""" Force Assign New Dataset """
-		if force:
-			self.__data = value
-		else:
-			raise ValueError("'force' must be manually set using the force flag")
+    def set_data(self, value, force=False):
+        """ Force Assign New Dataset """
+        if force:
+            self.__data = value
+        else:
+            raise ValueError("'force' must be manually set using the force flag")
 
-	@property 
-	def exports(self):
-		try:
-			return self.__exports
-		except:
-			self.export_data()
-			return self.__exports
-	@exports.setter
-	def exports(self, values):
-		self.__exports = values
+    @property 
+    def exports(self):
+        try:
+            return self.__exports
+        except:
+            self.export_data()
+            return self.__exports
+    @exports.setter
+    def exports(self, values):
+        self.__exports = values
 
-	@property 
-	def imports(self):
-		try:
-			return self.__imports
-		except:
-			self.import_data()
-			return self.__imports
-	@imports.setter
-	def imports(self, values):
-		self.__imports = values
+    @property 
+    def imports(self):
+        try:
+            return self.__imports
+        except:
+            self.import_data()
+            return self.__imports
+    @imports.setter
+    def imports(self, values):
+        self.__imports = values
 
 #-Exports-#
 
 class BACIExportData(BACI, CPExportData):
-	""" 
-	BACI Export Dataset
+    """ 
+    BACI Export Dataset
 
-	See Also
-	--------
-	pyeconlab.trade.dataset.CEPIIBACI.base
-	pyeconlab.trade.dataset.CPTradeData
+    See Also
+    --------
+    pyeconlab.trade.dataset.CEPIIBACI.base
+    pyeconlab.trade.dataset.CPTradeData
 
-	"""
-	__data = pd.DataFrame()
+    """
+    __data = pd.DataFrame()
 
-	@property 
-	def data(self):
-		return self.__data
-	# @data.setter
-	# def data(self, values):
-	# 	self.__data = values
+    @property 
+    def data(self):
+        return self.__data
+    # @data.setter
+    # def data(self, values):
+    #   self.__data = values
 
-	def set_data(self, value, force=False):
-		""" Force Assign New Dataset """
-		if force:
-			self.__data = value
-		else:
-			raise ValueError("'force' must be manually set using the force flag")
+    def set_data(self, value, force=False):
+        """ Force Assign New Dataset """
+        if force:
+            self.__data = value
+        else:
+            raise ValueError("'force' must be manually set using the force flag")
 
 #-Imports-#
 
 class BACIImportData(BACI, CPImportData):
-	""" 
-	BACI Import Dataset
+    """ 
+    BACI Import Dataset
 
-	See Also
-	--------
-	pyeconlab.trade.dataset.CEPIIBACI.base
-	pyeconlab.trade.dataset.CPTradeData
-	
-	"""
-	__data = pd.DataFrame()
+    See Also
+    --------
+    pyeconlab.trade.dataset.CEPIIBACI.base
+    pyeconlab.trade.dataset.CPTradeData
+    
+    """
+    __data = pd.DataFrame()
 
-	@property 
-	def data(self):
-		return self.__data
-	# @data.setter
-	# def data(self, values):
-	# 	self.__data = values
+    @property 
+    def data(self):
+        return self.__data
+    # @data.setter
+    # def data(self, values):
+    #   self.__data = values
 
-	def set_data(self, value, force=False):
-		""" Force Assign New Dataset """
-		if force:
-			self.__data = value
-		else:
-			raise ValueError("'force' must be manually set using the force flag")
+    def set_data(self, value, force=False):
+        """ Force Assign New Dataset """
+        if force:
+            self.__data = value
+        else:
+            raise ValueError("'force' must be manually set using the force flag")
