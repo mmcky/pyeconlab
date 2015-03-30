cc89cc02be416183e21d67a48d1b987b65ce50dd
Matthew McKay <sanguineturtle@users.noreply.github.com>
2015-03-31 09:54:14 +1100

    restore temporarily country_concordance - needs proper removal

--- pyeconlab/util/concordance.py
+++ pyeconlab/util/concordance.py
@@ -8,65 +8,65 @@ A set of Concordance Utilities
 import pandas as _pd
 import numpy as _np
 
-# import countrycode as _cc       #This is the external: PyCountryCode Project
+import countrycode as _cc       #This is the external: PyCountryCode Project
 
-# def countryname_concordance(data, concord_vars=('countryname', 'iso3c'), rtn_type='series', verbose=False):
-#     """
-#     Compute a Country Name Concordance using package: ``pycountrycode``
+def countryname_concordance(data, concord_vars=('countryname', 'iso3c'), rtn_type='series', verbose=False):
+    """
+    Compute a Country Name Concordance using package: ``pycountrycode``
     
-#     Parameters
-#     ----------
-#     data            :   list(string)
-#                         List of Country Names
-#     concord_vars    :   tuple(string, string), optional(default=('countryname', iso3c'))
-#                         Specify variables to Concord
-#     rtn_type        :   str, optional(default='series')
-#                         Return Type (dict or indexed Pandas Series)
+    Parameters
+    ----------
+    data            :   list(string)
+                        List of Country Names
+    concord_vars    :   tuple(string, string), optional(default=('countryname', iso3c'))
+                        Specify variables to Concord
+    rtn_type        :   str, optional(default='series')
+                        Return Type (dict or indexed Pandas Series)
 
-#     Notes
-#     ------
-#     ``pycountrycode`` is going through a re-write so this will most likely break                  
+    Notes
+    ------
+    ``pycountrycode`` is going through a re-write so this will most likely break                  
 
-#     ..  Future Work
-#         -----------
-#         [1] Build Internal CountryCode Routines
-#     """
-#     def replace_nonstring(items):
-#         for idx, code in enumerate(items):
-#             if type(code) != str:
-#                 items[idx] = '.'
-#         return items
+    ..  Future Work
+        -----------
+        [1] Build Internal CountryCode Routines
+    """
+    def replace_nonstring(items):
+        for idx, code in enumerate(items):
+            if type(code) != str:
+                items[idx] = '.'
+        return items
 
-#     def reject_non3digit(items):
-#         for idx, code in enumerate(items):
-#             if len(code) != 3:
-#                 items[idx] = '.'
-#         return items
+    def reject_non3digit(items):
+        for idx, code in enumerate(items):
+            if len(code) != 3:
+                items[idx] = '.'
+        return items
 
-#     if type(data) != list:
-#         raise TypeError("data: needs to be a list")
-#     #-Find Set Of Countries-#
-#     if type(data) == list:
-#         countrynames = list(set(data))
-#     if concord_vars[1] == 'iso3c':
-#         iso3c = _cc.countrycode(codes=countrynames, origin='country_name', target='iso3c')
-#         iso3c = replace_nonstring(iso3c)                                                    #Could use lambda functions
-#         iso3c = reject_non3digit(iso3c)                                                     #Could use lambda functions
-#         concord = _pd.Series(iso3c, index=countrynames, name='iso3c')
-#         concord.index.name = 'countryname'
-#         concord.sort()
-#     if concord_vars[1] == 'iso3n':
-#         iso3n = _cc.countrycode(codes=countrynames, origin='country_name', target='iso3n')
-#         iso3n = replace_nonstring(iso3n)
-#         iso3n = reject_non3digit(iso3n)
-#         concord = _pd.Series(iso3n, index=countrynames, name='iso3n')
-#         concord.index.name = 'countryname'
-#         concord.sort()
-#     #-Parse rtn_type-#
-#     if rtn_type == 'series':
-#         return concord
-#     else:
-#         return concord.to_dict()
+    if type(data) != list:
+        raise TypeError("data: needs to be a list")
+    #-Find Set Of Countries-#
+    if type(data) == list:
+        countrynames = list(set(data))
+    if concord_vars[1] == 'iso3c':
+        iso3c = _cc.countrycode(codes=countrynames, origin='country_name', target='iso3c')
+        iso3c = replace_nonstring(iso3c)                                                    #Could use lambda functions
+        iso3c = reject_non3digit(iso3c)                                                     #Could use lambda functions
+        concord = _pd.Series(iso3c, index=countrynames, name='iso3c')
+        concord.index.name = 'countryname'
+        concord.sort()
+    if concord_vars[1] == 'iso3n':
+        iso3n = _cc.countrycode(codes=countrynames, origin='country_name', target='iso3n')
+        iso3n = replace_nonstring(iso3n)
+        iso3n = reject_non3digit(iso3n)
+        concord = _pd.Series(iso3n, index=countrynames, name='iso3n')
+        concord.index.name = 'countryname'
+        concord.sort()
+    #-Parse rtn_type-#
+    if rtn_type == 'series':
+        return concord
+    else:
+        return concord.to_dict()
 
 
 def concord_data(concordance, item, issue_error=True, exception=False):
