d2d6a4303fd76d90b2d8f3f0e1efee65bd30e3f8
Matthew McKay <sanguineturtle@users.noreply.github.com>
2015-05-25 19:46:20 +1000

    Adding trade dataframe utilities

--- pyeconlab/trade/util/__init__.py
+++ pyeconlab/trade/util/__init__.py
@@ -3,3 +3,4 @@ from .pandas_converters import from_dict_to_dataframe, from_dict_of_series_to, r
 from .dynamics import compute_product_changes
 from .dynamic_converters import reindex_dynamic_dataframe, compute_persistence, reindex_dynamic_dict
 from .network import compute_average_centrality, compute_diffusion_properties_nx, construct_network_from_adjacency_df
+from .dataframe import attach_attributes
